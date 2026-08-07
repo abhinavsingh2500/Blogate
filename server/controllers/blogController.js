6600b80b395b1cc95a5580cbf82493243e68e129
abhinavsingh2500 <abhinavsingh250000@gmail.com>
2026-08-08 04:30:02 +0530

    Adding blog, deleting it, or doing any kind of operation

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -81,7 +81,7 @@ export const addBlog = async (req, res) => {
 
 export const getAllBlogs = async (req, res) => {
     try {
-        const blogs = await Blog.find({ isPublished: true });
+        const blogs = await Blog.find({});
         res.json({
             success: true,
             blogs
@@ -93,5 +93,74 @@ export const getAllBlogs = async (req, res) => {
         });
     }
 };
+
+export const getBlogById = async (req, res) => {
+    try {
+        const { blogId } = req.params;
+        const blog = await Blog.findById(blogId);
+        if (!blog) {
+            return res.json({
+                success: false,
+                message: "Blog not found"
+            });
+        }
+        res.json({
+            success: true,
+            blog
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+export const deleteBlogById = async (req, res) => {
+    try {
+        const { id } = req.body;
+        const blog = await Blog.findByIdAndDelete(id);
+        if (!blog) {
+            return res.json({
+                success: false,
+                message: "Blog not found"
+            });
+        }
+        res.json({
+            success: true,
+            message: "Blog deleted successfully"
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+export const togglePublish = async (req, res) => {
+    try {
+        const { id } = req.body;
+        const blog = await Blog.findById(id);
+        if (!blog) {
+            return res.json({
+                success: false,
+                message: "Blog not found"
+            });
+        }
+        blog.isPublished = !blog.isPublished;
+        await blog.save();
+        res.json({
+            success: true,
+            message: "Blog publish status toggled successfully"
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
 
 
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { addBlog } from "../controllers/blogController.js";
+import { addBlog, getAllBlogs, getBlogById, togglePublish, deleteBlogById } from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
 import auth from "../middleware/auth.js";
  
 const blogRouter = express.Router();
 
 blogRouter.post('/add', auth, upload.single('image'), addBlog);
+blogRouter.get('/all', getAllBlogs);
+blogRouter.get('/get', getAllBlogs);
+blogRouter.post('/delete', auth, deleteBlogById);
+blogRouter.get('/:blogId', getBlogById);
+blogRouter.post('/toggle-publish', auth, togglePublish);
 
 export default blogRouter;
+
 
